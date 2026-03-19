ca2cc8fc809e69cca4fcbb5e6dcfe90a1aaa2b03
Sinchi1 <goshat17@gmail.com>
2026-03-19 22:55:57 +0300

    project strcutrute

---
+++ bank/src/main/java/org/truskovski/BankMain.java
@@ -0,0 +1,12 @@
+package org.truskovski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMain {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BankMain.class, args);
+    }
+}
